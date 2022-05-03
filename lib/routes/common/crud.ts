209850295e0bb563b79cc21db9a2f3dd1f2e3cd4
roshan <roshan.poddar@geniusteacher.in>
2022-05-03 18:08:03 +0530

    Added some crud routes

--- lib/routes/common/crud.ts
+++ lib/routes/common/crud.ts
@@ -10,14 +10,15 @@ export class Crud {
     crudRoutes = async (req: Request, res: Response,schema) => {
         let route = req.params.route
         if(route == 'fetch'){
-            console.log(req.body)
-            if(Object.keys(req.body.filter).includes('_id'))
-                req.body.filter['_id'] = new mongoose.Types.ObjectId(req.body.filter['_id'])
             let persistable = await new Persistable(schema).fetch(req.body)
             res.send(persistable)
         }
         if(route == 'upsert'){
             let persistable = await new Persistable(schema).upsert(req.body)
+            res.send(persistable)
+        }
+        if(route == 'count'){
+            let persistable = await new Persistable(schema).count(req.body)
             res.send(persistable)
         }
     } 
